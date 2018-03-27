72643294e66239b01b1e9dec2e2650b8ec712cdc
Franco Saworski <franco.saworski@blik.io>
2018-03-27 13:51:14 +0200

    feat: initial akita_gen2 board support
    * ported from frdm_kw41z
    * changed pinmuxing, dts, board related names

---
+++ boards/arm/akita_gen2/board.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018, blik GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+#define BUT0_GPIO_NAME	CONFIG_GPIO_MCUX_PORTC_NAME
+#define BUT0_GPIO_PIN	2
+
+#define LED0_GPIO_PORT	CONFIG_GPIO_MCUX_PORTA_NAME
+#define LED0_GPIO_PIN	19
+
+#endif /* __INC_BOARD_H */
