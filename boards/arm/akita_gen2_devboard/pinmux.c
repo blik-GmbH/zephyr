d67bebca7c59ecd167e81943439cbc80fe77dad3
Tobias Aschenbrenner <taschenb@posteo.de>
2018-04-16 10:43:09 +0200

    refactor: akita_gen2_devboard: Remove unused pins from pinmuxing

--- boards/arm/akita_gen2_devboard/pinmux.c
+++ boards/arm/akita_gen2_devboard/pinmux.c
@@ -25,13 +25,6 @@ static int akita_gen2_devboard_pinmux_init(struct device *dev)
 		device_get_binding(CONFIG_PINMUX_MCUX_PORTC_NAME);
 #endif
 
-	/* Red, green, blue LEDs. Note the red LED and accel INT1 are both
-	 * wired to PTC1.
-	 */
-	pinmux_pin_set(portc,  1, PORT_PCR_MUX(kPORT_MuxAsGpio));
-	pinmux_pin_set(porta, 19, PORT_PCR_MUX(kPORT_MuxAsGpio));
-	pinmux_pin_set(porta, 18, PORT_PCR_MUX(kPORT_MuxAsGpio));
-
 #if CONFIG_I2C_1
 	/* I2C1 SCL, SDA */
 	pinmux_pin_set(portc,  2, PORT_PCR_MUX(kPORT_MuxAlt3)
@@ -52,10 +45,6 @@ static int akita_gen2_devboard_pinmux_init(struct device *dev)
 	/* ADC0_SE3 */
 	pinmux_pin_set(portb,  2, PORT_PCR_MUX(kPORT_PinDisabledOrAnalog));
 #endif
-
-	/* SW3, SW4 */
-	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAsGpio));
-	pinmux_pin_set(portc,  5, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
 #ifdef CONFIG_UART_MCUX_LPUART_0
 	/* UART0 RX, TX */
