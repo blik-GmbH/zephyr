47ffb48998ccd5ae983c127e5b3209345e673c4e
Franco Saworski <franco.saworski@blik.io>
2018-05-18 11:34:26 +0200

    feat: board: add MAC + UID defines
    We want to use the MAC and UID for device identification. This patch set
    adds defines to the board headers of our AKITA Gen2 boards. The comments
    document the bitmap layout.
    
    Signed-off-by: Franco Saworski <franco.saworski@posteo.de>

--- boards/arm/akita_gen2_devboard/board.h
+++ boards/arm/akita_gen2_devboard/board.h
@@ -23,4 +23,59 @@
 #define SW0_GPIO_NAME	SW3_GPIO_NAME
 #define SW0_GPIO_PIN	SW3_GPIO_PIN
 
+/*
+ * @brief MAC_LSB / MAC_0
+ *
+ * @note The first 32bit group and least significant part of the 40bit MAC
+ *
+ * 32bit bitfield:
+ * 31-00: MAC
+ */
+#define AKITA_MAC_0 ((uint32_t)(RSIM->MAC_LSB))
+
+/*
+ * @brief MAC_MSB / MAC_1
+ *
+ * @note The second 32bit group with its lower 8bit used and upper 24bit zero,
+ *       and most significant part of the 40bit UID
+ *
+ * 32bit bitfield:
+ * 31-08: zero
+ * 07-00: MAC
+ */
+#define AKITA_MAC_1 ((uint32_t)(RSIM->MAC_MSB))
+
+/*
+ * @brief UIDL / UID_0
+ *
+ * @note The first 32bit group and least significant part of the 80bit UID
+ *
+ * 32bit bitfield:
+ * 31-00: UID
+ */
+#define AKITA_UID_0 ((uint32_t)(SIM->UIDL))
+
+/*
+ * @brief UIDML / UID_1
+ *
+ * @note The second 32bit group between least and most significant part of the
+ *       80bit UID
+ *
+ * 32bit bitfield:
+ * 31-00: UID
+ */
+#define AKITA_UID_1 ((uint32_t)(SIM->UIDML))
+
+/*
+ * @brief UIDMH / UID_2
+ *
+ * @note The third 32bit group with its lower 16bit used and upper 16bit unused,
+ *       and most significant part of the 80bit UID
+ *
+ * 32bit bitfield:
+ * 31-16: reserved
+ * 15-00: UID
+ */
+#define AKITA_UID_2 ((uint32_t)(SIM->UIDMH))
+
 #endif /* __INC_BOARD_H */
