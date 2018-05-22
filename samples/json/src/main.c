918c3eb6d3da275de6fa421822f0ae97cd1e6c02
Alexander Preißner <alexander.preissner@blik.io>
2018-05-22 15:01:06 +0200

    fix: JSON sample: corrected formatting
    * Correction of indentations and locations of opening braces.

--- samples/json/src/main.c
+++ samples/json/src/main.c
@@ -35,7 +35,6 @@
  */
 #define WRITE_TO_FILE           0
 
-
 //------------------------------------------------------------------------------
 
 #define MEM_SLAB_SIZE           1024
@@ -46,8 +45,7 @@ K_MEM_SLAB_DEFINE(mem_slab, MEM_SLAB_SIZE, 2, 4);
 /**
  * @brief C struct containing device configuration items
  */
-struct device_configuration_struct
-{
+struct device_configuration_struct {
     uint32_t rf_tx_channel;
     uint32_t rf_rx_channel;
     uint32_t rf_pan_id;
@@ -64,54 +62,51 @@ struct device_configuration_struct
 /**
  * @brief List of JSON object descriptors for #device_configuration_struct
  */
-static const struct json_obj_descr device_configuration_descr[] =
-{
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-            rf_tx_channel, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-            rf_rx_channel, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-            rf_pan_id, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_tx_power, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_tx_interval, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_rx_interval, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_nd_interval, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_ca_1, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_ca_2, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                rf_ca_3, JSON_TOK_NUMBER),
-    JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
-                pktopt, JSON_TOK_NUMBER)
-};
-
+static const struct json_obj_descr device_configuration_descr[] = {
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_tx_channel, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_rx_channel, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_pan_id, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_tx_power, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_tx_interval, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_rx_interval, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_nd_interval, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_ca_1, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_ca_2, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        rf_ca_3, JSON_TOK_NUMBER),
+JSON_OBJ_DESCR_PRIM(struct device_configuration_struct,
+        pktopt, JSON_TOK_NUMBER) };
 
 // Extra appended JSON object simulates extension of device configuration in
 // later firmware revisionS and what happens on older devices.
-const char excess_json_string[]    = ",\"other_field\":123456}";
-const size_t excess_json_string_len = sizeof(excess_json_string);
+const char excess_json_string[] = ",\"other_field\":123456}";
+const size_t excess_json_string_len = sizeof (excess_json_string);
 
-void main(void)
-{
+void main(void) {
     printf("=============================================================\n");
     printf("JSON on NFFS test\n");
     printf("=============================================================\n\n");
 
-	fs_file_t file_p;
-	int ret;
+    fs_file_t file_p;
+    int ret;
 
-	//--------------------------------------------------------------------------
-	// Erase Flash device
-	//--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    // Erase Flash device
+    //--------------------------------------------------------------------------
 
 #if ERASE_DEVICE
-	// Erase device if configured
-    struct device *flash_dev = device_get_binding(CONFIG_SPI_FLASH_W25QXXXX_DRV_NAME);
+    // Erase device if configured
+    struct device *flash_dev = device_get_binding(
+            CONFIG_SPI_FLASH_W25QXXXX_DRV_NAME);
     flash_write_protection_set(flash_dev, false);
     flash_erase(flash_dev, 0, CONFIG_SPI_FLASH_W25QXXXX_FLASH_SIZE);
     return;
@@ -132,8 +127,7 @@ void main(void)
             .rf_ca_1 = 7,
             .rf_ca_2 = 8,
             .rf_ca_3 = 9,
-            .pktopt = 0xcafe
-    };
+            .pktopt = 0xcafe};
 
     //--------------------------------------------------------------------------
     // Encode as JSON string
@@ -145,29 +139,25 @@ void main(void)
 
     // Allocate a buffer of appropriate size
     uint8_t *buf;
-    if(k_mem_slab_alloc(&mem_slab, (void *)&buf, 100) != 0)
-    {
+    if (k_mem_slab_alloc(&mem_slab, (void *) &buf, 100) != 0) {
         printf("Memory slab allocation timed out\n");
         k_sleep(100);
         return;
     }
-    else
-    {
+    else {
         printf("Allocated a memory slab\n");
     }
 
     printf("Encode data into JSON string:\n");
     ret = json_obj_encode_buf(device_configuration_descr,
             ARRAY_SIZE(device_configuration_descr), &dc, buf, MEM_SLAB_SIZE);
-    if(ret)
-    {
+    if (ret) {
         printf("ERROR: JSON encoding failed\n");
         k_sleep(100);
         return;
     }
-    else
-    {
-        for(int i = 0; i < enc_len; i++){
+    else {
+        for (int i = 0; i < enc_len; i++) {
             printf("%c", buf[i]);
         }
         printf("\n");
@@ -180,14 +170,12 @@ void main(void)
     printf("Parsing JSON string\n");
     ret = json_obj_parse(buf, enc_len, device_configuration_descr,
             ARRAY_SIZE(device_configuration_descr), &test_dc);
-    if(ret != (1 << ARRAY_SIZE(device_configuration_descr)) - 1)
-    {
+    if (ret != (1 << ARRAY_SIZE(device_configuration_descr)) - 1) {
         printf("ERROR: JSON parsing failed: %d\n", ret);
         k_sleep(100);
         return;
     }
-    else
-    {
+    else {
         printf("RF TX Channel: %d\n", test_dc.rf_tx_channel);
         printf("RF RX Channel: %d\n", test_dc.rf_rx_channel);
         printf("RF PAN ID: 0x%04x\n", test_dc.rf_pan_id);
@@ -199,7 +187,7 @@ void main(void)
     printf("Add additional unspecified JSON object to string:\n");
     memcpy(&buf[enc_len - 1], &excess_json_string, excess_json_string_len);
     enc_len += excess_json_string_len;
-    for(int i = 0; i < enc_len; i++){
+    for (int i = 0; i < enc_len; i++) {
         printf("%c", buf[i]);
     }
     printf("\n");
@@ -209,98 +197,83 @@ void main(void)
     //--------------------------------------------------------------------------
 #if WRITE_TO_FILE
     // Create file in root directory
-	// Don't forget the forward slash '/' that denotes the root directory.
+    // Don't forget the forward slash '/' that denotes the root directory.
     // Else, file creation will fail.
-	ret = fs_open(&file_p, "/config.txt");
-	if(ret)
-	{
-	    printf("Could not open file\n");
-	    k_sleep(100);
-	    return;
-	}
-	else
-	{
-	    printf("File open\n");
-	}
-
-	ret = fs_write(&file_p, buf, enc_len);
-	if(ret != enc_len)
-	{
-	    printf("Something went wrong during writing: %d\n", ret);
-	    k_sleep(100);
-	    return;
-	}
-	else
-	{
-	    printf("Wrote string to file\n");
-	}
-	k_sleep(100);
-
-	ret = fs_close(&file_p);
-	if(ret)
-	{
-	    printf("Could not close file\n");
-	}
-	else
-	{
-	    printf("File closed\n");
-	}
-	k_sleep(100);
-#endif
-
-	//--------------------------------------------------------------------------
-	// Read JSON string back from file
-	//--------------------------------------------------------------------------
-	memset(buf, 0, sizeof(buf));
-	// Need to find out the file size in order to read right number of bytes.
-	struct fs_dirent config_file_info;
-	ret = fs_stat("/config.txt", &config_file_info);
-	if(ret)
-	{
-	    printf("ERROR: Could not get JSON file info\n");
-	    return;
-	}
-	else
-	{
-	    printf("JSON file is %u bytes long\n", config_file_info.size);
-	}
-
-
-	ret = fs_open(&file_p, "/config.txt");
-    if(ret)
-    {
+    ret = fs_open(&file_p, "/config.txt");
+    if (ret) {
         printf("Could not open file\n");
         k_sleep(100);
         return;
     }
-    else
-    {
+    else {
+        printf("File open\n");
+    }
+
+    ret = fs_write(&file_p, buf, enc_len);
+    if (ret != enc_len) {
+        printf("Something went wrong during writing: %d\n", ret);
+        k_sleep(100);
+        return;
+    }
+    else {
+        printf("Wrote string to file\n");
+    }
+    k_sleep(100);
+
+    ret = fs_close(&file_p);
+    if (ret) {
+        printf("Could not close file\n");
+    }
+    else {
+        printf("File closed\n");
+    }
+    k_sleep(100);
+#endif
+
+    //--------------------------------------------------------------------------
+    // Read JSON string back from file
+    //--------------------------------------------------------------------------
+    memset(buf, 0, sizeof (buf));
+    // Need to find out the file size in order to read right number of bytes.
+    struct fs_dirent config_file_info;
+    ret = fs_stat("/config.txt", &config_file_info);
+    if (ret) {
+        printf("ERROR: Could not get JSON file info\n");
+        return;
+    }
+    else {
+        printf("JSON file is %u bytes long\n", config_file_info.size);
+    }
+
+    ret = fs_open(&file_p, "/config.txt");
+    if (ret) {
+        printf("Could not open file\n");
+        k_sleep(100);
+        return;
+    }
+    else {
         printf("File open\n");
     }
 
     ret = fs_read(&file_p, buf, config_file_info.size);
-    if(ret != config_file_info.size)
-    {
+    if (ret != config_file_info.size) {
         printf("Something went wrong during read: %d\n", ret);
         k_sleep(100);
         return;
     }
-    else
-    {
+    else {
         printf("Read from file:\n");
-        for(int i = 0; i < config_file_info.size; i++){
+        for (int i = 0; i < config_file_info.size; i++) {
             printf("%c", buf[i]);
         }
         printf("\n");
     }
 
     ret = fs_close(&file_p);
-    if(ret)
-    {
+    if (ret) {
         printf("Could not close file\n");
     }
-    else
-    {
+    else {
         printf("File closed\n");
     }
 
@@ -311,31 +284,29 @@ void main(void)
     printf("Parsing JSON string\n");
     ret = json_obj_parse(buf, config_file_info.size, device_configuration_descr,
             ARRAY_SIZE(device_configuration_descr), &test_dc2);
-    if(ret != (1 << ARRAY_SIZE(device_configuration_descr)) - 1)
-    {
+    if (ret != (1 << ARRAY_SIZE(device_configuration_descr)) - 1) {
         printf("ERROR: JSON parsing failed: %d\n", ret);
         k_sleep(100);
         return;
     }
-    else
-    {
-        printf("RF TX Channel: %u\n",   test_dc2.rf_tx_channel);
-        printf("RF RX Channel: %u\n",   test_dc2.rf_rx_channel);
-        printf("RF PAN ID: 0x%04x\n",   test_dc2.rf_pan_id);
-        printf("RF TX Power: %d\n",     test_dc2.rf_tx_power);
-        printf("RF TX Interval: %u\n",  test_dc2.rf_tx_interval);
-        printf("RF RX Interval: %u\n",  test_dc2.rf_rx_interval);
-        printf("RF ND Interval: %u\n",  test_dc2.rf_nd_interval);
-        printf("RF CA Param 1: %d\n",   test_dc2.rf_ca_1);
-        printf("RF CA Param 2: %d\n",   test_dc2.rf_ca_2);
-        printf("RF CA Param 3: %d\n",   test_dc2.rf_ca_3);
-        printf("PKTOPT: 0x%08x\n",      test_dc2.pktopt);
+    else {
+        printf("RF TX Channel: %u\n", test_dc2.rf_tx_channel);
+        printf("RF RX Channel: %u\n", test_dc2.rf_rx_channel);
+        printf("RF PAN ID: 0x%04x\n", test_dc2.rf_pan_id);
+        printf("RF TX Power: %d\n", test_dc2.rf_tx_power);
+        printf("RF TX Interval: %u\n", test_dc2.rf_tx_interval);
+        printf("RF RX Interval: %u\n", test_dc2.rf_rx_interval);
+        printf("RF ND Interval: %u\n", test_dc2.rf_nd_interval);
+        printf("RF CA Param 1: %d\n", test_dc2.rf_ca_1);
+        printf("RF CA Param 2: %d\n", test_dc2.rf_ca_2);
+        printf("RF CA Param 3: %d\n", test_dc2.rf_ca_3);
+        printf("PKTOPT: 0x%08x\n", test_dc2.pktopt);
     }
 
     //--------------------------------------------------------------------------
     // Free memory slab
     //--------------------------------------------------------------------------
-    k_mem_slab_free(&mem_slab, (void *)&buf);
+    k_mem_slab_free(&mem_slab, (void *) &buf);
 
-	printf("End of application\n");
+    printf("End of application\n");
 }
